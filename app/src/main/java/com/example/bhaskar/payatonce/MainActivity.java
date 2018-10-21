a7d3409e865667254b9dbfdcb6a7a17f5d79a928
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2018-10-22 05:23:11 +0530

    done

--- app/src/main/java/com/example/bhaskar/payatonce/MainActivity.java
+++ app/src/main/java/com/example/bhaskar/payatonce/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.bhaskar.payatonce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    double total = 0;
+    TextView tot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tot = (TextView)findViewById(R.id.tot);
     }
 
     public void pay(View view) {
-    }
-
-    public void add(View view) {
+        Intent intent = new Intent(this,FinalResult.class);
+        startActivity(intent);
     }
 }
