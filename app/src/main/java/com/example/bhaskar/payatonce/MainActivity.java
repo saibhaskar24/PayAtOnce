eee6eb1733dc6a4ccab76fd08e5abcab4b2835c5
Sai Sampath Kumar <balivadask2000@gmail.com>
2018-10-22 06:56:31 +0530

    1:55 Firebase

--- app/src/main/java/com/example/bhaskar/payatonce/MainActivity.java
+++ app/src/main/java/com/example/bhaskar/payatonce/MainActivity.java
@@ -1,25 +1,27 @@
 package com.example.bhaskar.payatonce;
 
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    double total =0;
+    TextView tot ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tot = (TextView)findViewById(R.id.tot);
     }
 
     public void pay(View view) {
-        setContentView(R.layout.successful);
-    }
+        Intent intent = new Intent(this,FinalResult.class);
+        startActivity(intent);
+  }
 
-    public void add(View view) {
-    }
 }
