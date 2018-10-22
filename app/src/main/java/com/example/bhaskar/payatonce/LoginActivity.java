2ed1be928cf9d53e315f55e475da632092b6f249
sai bhaskar <35323355+saibhaskar24@users.noreply.github.com>
2018-10-22 17:10:53 +0530

    string

--- app/src/main/java/com/example/bhaskar/payatonce/LoginActivity.java
+++ app/src/main/java/com/example/bhaskar/payatonce/LoginActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import android.content.CursorLoader;
 import android.content.Loader;
@@ -48,70 +49,68 @@ import java.util.List;
 import static android.Manifest.permission.READ_CONTACTS;
 
 
-public class LoginActivity extends AppCompatActivity  {
+public class LoginActivity extends AppCompatActivity {
     String url = "http://200899a0.ngrok.io/WebApplication1/api/Products/";
     EditText pass;
     AutoCompleteTextView ph;
-    String passwd,bal,el,w,m,tv,ga;
-    JSONObject root;
+    RequestQueue requestQueue;
+    String passwd, bal, el, w, m, tv, ga;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        requestQueue = Volley.newRequestQueue(this);
 
-        pass = (EditText)findViewById(R.id.password);
-        ph = (AutoCompleteTextView)findViewById(R.id.phno);
-        }
 
-    public void signin(View view ) throws IOException {
+        pass = (EditText) findViewById(R.id.password);
+        ph = (AutoCompleteTextView) findViewById(R.id.phno);
+    }
+
+    public void signin(View view) throws IOException {
         String phonno = ph.getText().toString();
         url = url + phonno;
         fetchData();
-        if(pass.getText().toString() == passwd) {
+        if (pass.getText().toString() == passwd) {
             Intent intent = new Intent(this, MainActivity.class);
-            intent.putExtra("bal",bal);
-            intent.putExtra("el",el);
-            intent.putExtra("w",w);
-            intent.putExtra("tv",tv);
-            intent.putExtra("ga",ga);
+            intent.putExtra("bal", bal);
+            intent.putExtra("el", el);
+            intent.putExtra("w", w);
+            intent.putExtra("tv", tv);
+            intent.putExtra("ga", ga);
 
             startActivity(intent);
-        }
-        else {
+        } else {
             //Show wrong passwd
         }
     }
-    public void fetchData() throws IOException {
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+    public void fetchData()  {
+        StringRequest s1 = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject root= new JSONObject(response);
+                    passwd = root.getString("password");
+                    bal =root.getString("BankBal");
+                    el = root.getString("Electricity");
+                    w = root.getString("Water");
+                    m=root.getString("Mobile");
+                    tv=root.getString("Tv");
+                    ga=root.getString("Gas");
 
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                       JSONObject root= new JSONObject(response);
-                        try {
-                            passwd = root.getString("password");
-                            bal =root.getString("BankBal");
-                            el=root.getString("Electricity");
-                            w=root.getString("Water");
-                            m=root.getString("Mobile");
-                            tv=root.getString("Tv");
-                            ga=root.getString("Gas");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Rest Response", error.toString());
-                    }
+                } catch (JSONException e) {
+
+
                 }
-        );
-        requestQueue.add(objectRequest);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(s1);
     }
 }
-
